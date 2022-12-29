9723b2d0affda1aef6c30e1b67a3fe19f29449f1
Mayank <mayankjhanwar29@gmail.com>
2022-12-29 18:10:14 +0530

    frontend to the application

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import {styled} from "@mui/system"
+
+const FlexBetween = styled(Box)({
+    display:"flex",
+    justifyContent:"space-between" , 
+    alignItems:"center"
+})
+// these kinds of syntax are not usually used but you can utileize it when you are using css as the component 
+
+export default FlexBetween ; 
+
